b54e33dbc71f50d3231fe7fa8f780872edee5a5a
Mateusz Paprocki <mattpap@gmail.com>
2018-06-02 00:26:10 +0200

    Improved resolution when mod.js and mod/index.js both exist

--- bokehjs/make/linker.ts
+++ bokehjs/make/linker.ts
@@ -230,15 +230,19 @@ export class Linker {
       return path
 
     const file = path + this.ext
-    if (exists(file) && is_file(file))
-      return file
-    else if (exists(path) && is_dir(path)) {
-      const file = join(path, "index" + this.ext)
-      if (exists(file) && is_file(file))
-        return file
-    }
+    const has_file = exists(file) && is_file(file)
 
-    throw new Error(`can't resolve '${dep}' from '${parent.file}'`)
+    const index = join(path, "index" + this.ext)
+    const has_index = exists(index) && is_file(index)
+
+    if (has_file && has_index)
+      throw new Error(`both ${file} and ${index} exist, remove one of them or clean the build and recompile`)
+    else if (has_file)
+      return file
+    else if (has_index)
+      return index
+    else
+      throw new Error(`can't resolve '${dep}' from '${parent.file}'`)
   }
 
   protected resolve_absolute(dep: string, parent: Module): string {
